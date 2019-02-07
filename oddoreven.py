238c1ee6a40393bbbb7a52b999b67e54f1351a78
Naveenn06 <47408912+Naveenn06@users.noreply.github.com>
2019-02-07 13:52:10 +0530

    Update and rename oddoreven.py to odorev.py

--- oddoreven.py
+++
@@ -1,5 +0,0 @@
-a=int(input("Please Enter a Number : "));  
-if(a%2==0):  
-    print("This Number is Even")  
-else:  
-    print("This Number is Odd")  
---
+++ odorev.py
@@ -0,0 +1,15 @@
+number=int(input("Please Enter a Number : "));  
+x=int(number/2)*2;  
+if(x==number):  
+    print("Tis Number is Even")    
+else:  
+    print("Tis Number is Odd")    
+
+
+    
+    
+    
+    
+    
+
+    
