da1aa9aa9eb812442fb018881173990b2d2d1382
Naveenn06 <47408912+Naveenn06@users.noreply.github.com>
2019-02-07 14:39:05 +0530

    Update voworcon.py

--- voworcon.py
+++ voworcon.py
@@ -4,4 +4,4 @@ if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
     print(ch, "is a Vowl")
 else:
-    print(ch, "is a Consont")
+    print(ch, "is a Consont"
